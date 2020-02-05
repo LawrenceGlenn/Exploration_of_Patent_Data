8fd7b5efa64a5e0d220ea71030e0d21516e07e66
Lawrence <Lawrence.O.Glenn@gmail.com>
2020-02-05 15:51:05 -0800

    renamed tfidf analysis to nmf analysis and created top words and display functions

---
+++ src/NMF_Analysis.py
@@ -0,0 +1,34 @@
+from sklearn.decomposition import NMF
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def plot_nmf_tfidf(ax, matrix, num):
+
+	error = [fit_nmf_tfidf(matrix, i).reconstruction_err_ for i in range(1,num+1)]
+	ax.plot(range(1,num+1), error)
+	ax.set_xticks(range(1, num+1))
+	ax.set_xlabel('r')
+	ax.set_ylabel('Reconstruction Error')
+
+
+def fit_nmf_tfidf(matrix, r):
+    nmf = NMF(n_components=r)
+    nmf.fit(matrix)
+    return nmf
+
+def display_W_H(W,H,index,columns,comp):
+
+	# Make interpretable
+	W, H = (np.around(x,comp) for x in (W,H))
+	W = pd.DataFrame(W,index=index)
+	H = pd.DataFrame(H,columns=columns)
+
+	display(W) 
+	display(H)
+
+def top_words(H, columns, num):
+	H = pd.DataFrame(H,columns=columns)
+	for i in range(H.shape[0]):
+		print(H.sort_values(by=i, ascending=False,axis=1).iloc[i:i+1,:num])
+		
