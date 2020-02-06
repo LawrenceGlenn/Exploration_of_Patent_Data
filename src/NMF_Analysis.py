4f4f3534765659c3788a45fbe4ff2cd569d387a8
Lawrence <Lawrence.O.Glenn@gmail.com>
2020-02-05 19:42:48 -0800

    added jaccard testing to nmf component analysis

--- src/NMF_Analysis.py
+++ src/NMF_Analysis.py
@@ -3,13 +3,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def plot_nmf_tfidf(ax, matrix, num):
+def plot_nmf_tfidf(ax, matrix, num, diff=""):
 
-	error = [fit_nmf_tfidf(matrix, i).reconstruction_err_ for i in range(1,num+1)]
+	error = compute_error(matrix,num,diff)
 	ax.plot(range(1,num+1), error)
 	ax.set_xticks(range(1, num+1))
 	ax.set_xlabel('r')
 	ax.set_ylabel('Reconstruction Error')
+
+def compute_error(matrix,num, diff=""):
+	if diff == "jaccard":
+		return [compute_jaccard_similarity(matrix,i) for i in range(1,num+1)]
+	else:
+		return [fit_nmf_tfidf(matrix,i).reconstruction_err_ for i in range(1,num+1)]
+
+def compute_jaccard_similarity(matrix,i ):
+	nmf = fit_nmf_tfidf(matrix,i)
+	H = nmf.components_
+	H = pd.DataFrame(H,columns=matrix.columns)
+
+	output = []
+	for i in range(len(H)-1):
+		x= H[H!=0.0].iloc[i]
+		x = x[~np.isnan(x)]
+		x=set(x.index)
+		y= H[H!=0.0].iloc[i+1]
+		y = y[~np.isnan(y)]
+		y=set(y.index)
+		intersection = set(x).intersection(set(y))
+		union = set(x).union(set(y))
+		output.append(len(intersection)/len(union))
+	return np.mean(output)
 
 
 def fit_nmf_tfidf(matrix, r):
--- src/Tfidf_on_Pandas.py
+++ src/Tfidf_on_Pandas.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 def tfidf_vector_matrix(df,ngrams_start,ngrams_stop):
 	corpus = [" ".join(row) for row in df['abstract_lemmed']]
-	tfidf = TfidfVectorizer(ngram_range=(ngrams_start,ngrams_stop))
+	tfidf = TfidfVectorizer(ngram_range=(ngrams_start,ngrams_stop),min_df=.01,max_df=.6)
 	document_tfidf_matrix = tfidf.fit_transform(np.array(corpus))
 
 	return pd.DataFrame(document_tfidf_matrix.toarray(), columns= tfidf.get_feature_names())
