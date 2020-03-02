aa73da568e6e53d07afd4c89747d4ffa503c9b87
LawrenceGlenn <Lawrence.O.Glenn@gmail.com>
2020-03-02 04:36:32 +0000

    produced multiple recall curves and updated readme

---
+++ src/EDA_plots.py
@@ -0,0 +1,11 @@
+
+
+def plot_feature_importance(ax, features, importances):
+    ax.xaxis.set_tick_params(labelsize=20)
+    ax.yaxis.set_tick_params(labelsize=20)
+    comb = dict(zip(features, importances))
+    comb = {k: v for k, v in sorted(comb.items(), key=lambda item: item[1],reverse=True)}
+    ax.bar(list(comb.keys())[:10],list(comb.values())[:10])
+    ax.set_xlabel('features', fontsize=34, labelpad=15)
+    for tick in ax.xaxis.get_major_ticks()[1::2]:
+        tick.set_pad(25)
